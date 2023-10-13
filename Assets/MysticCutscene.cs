861cf2b63b790c80413827852189fb749b99bc20
ToriSmash <Victoria.Mckenzie1@mds.ac.nz>
2023-10-13 14:17:20 +1300

    Revert "Added Background and Icons as well as controller functions"
    This reverts commit b26b1345274b4a0684515471d22cbabcb1207c31.

--- Assets/MysticCutscene.cs
+++ Assets/MysticCutscene.cs
@@ -7,26 +7,10 @@ using UnityEngine.PS4;
 
 public class MysticCutscene : MonoBehaviour
 {
-    [SerializeField] float lightHueChangeRate;
-    float lightHue = 0.0f;
+    
 
     void Update()
     {
-#if UNITY_PS4
-    if (PS4Input.PadIsConnected(0))
-    {
-        lightHue = Mathf.Repeat(lightHue += Time.deltaTime * lightHueChangeRate, 1.0f);
-        Color newColor = Color.HSVToRGB(lightHue, 1.0f, 1.0f);        
-    
-        //Change Gamepad Light Colour
-         PS4Input.PadSetLightBar(0, (int)(newColor.r * 255), (int)(newColor.g * 255), (int)(newColor.b * 255));
-    }
-
-#endif
-    }
-
-    void OnDestroy()
-    {
-
+        
     }
 }
